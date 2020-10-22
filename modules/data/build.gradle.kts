25c411461d8482f60cb07ee4b319d8bed135a7b8
aridai <aridai.net@outlook.jp>
2020-10-22 16:06:10 +0900

    ライブラリを追加
    * Retrofit
    * Moshi

--- modules/data/build.gradle.kts
+++ modules/data/build.gradle.kts
@@ -1,8 +1,14 @@
 dependencies {
+    implementation(project(":application"))
+
     implementation(Dependencies.Kotlin.stdlib)
     implementation(Dependencies.Kotlin.Coroutines.core)
     implementation(Dependencies.Kotlin.Coroutines.android)
     implementation(Dependencies.Koin.coreExt)
+    implementation(Dependencies.Moshi.adapters)
+    implementation(Dependencies.Moshi.kotlin)
+    implementation(Dependencies.Retrofit.converterMoshi)
+    implementation(Dependencies.Retrofit.retrofit)
     implementation(Dependencies.threeTenAbp)
     implementation(Dependencies.timber)
 }
